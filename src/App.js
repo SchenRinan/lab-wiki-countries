17d5714abd0cdcae1507eb8dc098acbbddb8f985
Rinan <jandrewschnell@gmail.com>
2023-08-02 13:53:57 +0200

    iteration 4

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import CountriesList from './components/CountriesList';
 import Navbar from './components/Navbar';
 import ErrorPage from './components/ErrorPage';
 import CountryDetails from './components/CountryDetails';
+import { useEffect, useState } from 'react';
 
-import countries from './countries.json'
+// import countries from './countries.json'
+
 
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://ih-countries-api.herokuapp.com/countries')
+       .then((res) => res.json())
+       .then((data) => {
+          setCountries(data);
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+ }, []);
+
   return (
     <div className="App">
       <Navbar/>
@@ -21,7 +35,10 @@ function App() {
           </div>
           <Routes>
             <Route path="/"/> 
+            {/* Iteration 3 */}
             {countries.map(element => <Route path={`/${element.alpha3Code}`} element={<CountryDetails country={element}/>} />)}
+            {/* Iteration 5 */}
+            {/* {countries.map(element => <Route path={`/${element.alpha3Code}`} element={<CountryDetails country={element.alpha3Code}/>} />)} */}
             <Route path="*" element={ <ErrorPage /> } />
           </Routes>
         </div>
